a6ff5e4b4482cb99be626ff55fe85c0bd9fe343f
root <root@116.37.167.72.host.secureserver.net>
2025-01-07 04:39:22 -0400

    Modified from command line

--- pre-includes/db.php
+++ pre-includes/db.php
@@ -1,7 +1,7 @@
 <?php
 // DATABASE CONNECTION //
 @session_start();
-$con = mysqli_connect( '127.0.0.1', 'root', '', 'remoteclinic');
+$con = mysqli_connect( '127.0.0.1', 'clinic', '12Aguacate34', 'clinic');
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
